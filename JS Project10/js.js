4a3a39b6e7a5e47adeb98b4b8e97de9a651c1bf4
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 18:26:00 +0300

    Java Script Project13

---
+++ JS Project10/js.js
@@ -0,0 +1,33 @@
+// let s = 0;
+// let arr = ["banan", "Tandz", "Xndzor", "bKiwi"]
+// for (let n = 0; n < arr.length; n++) {
+//     if (arr[n][0].toLowerCase() == "b") {
+//         s++
+//     }
+// }
+// alert(s)
+
+// let s = 0
+// let arr = ["laban", "Tanbz", "Xndbor", "Kiki"]
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//         if (arr[i][j].toLowerCase() == "b") {
+//             s++
+//         }
+//     }
+// }
+// alert(s)
+
+// let ages = [32, 33, 16, 40];
+// let result = ages.filter(check);
+// function check(age) {
+//     return age >= 18;
+// }
+// alert(result)
+
+// let arr = ["banan", "baobab", "baklajan", "kabachki"];
+// let result = arr.filter(check);
+// function check(fruit) {
+//     return fruit[0] === "b";
+// }
+// alert(result)
