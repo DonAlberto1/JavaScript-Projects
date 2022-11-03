4a57fe3e5ea7910d516aa9c3c33f508c0d993502
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 18:27:56 +0300

    Java Script Project14

---
+++ JS Project11/js.js
@@ -0,0 +1,58 @@
+//A task 1
+// let text ="HELLO WORLD"
+// let letter = text.charAt(1);
+// alert(letter);
+
+//A task 2
+// let text1 = "Hello";
+// let text2 = "world!";
+// let result = text1.concat("", text2, text1);
+// alert(result)
+
+//A task 3
+// let text = "Hello World,welcome to the universe.";
+// let result = text.includes("World");
+// alert(result)
+
+//A task 4
+// let text1 = "Hello eeee"
+// let text2 = "Barev"
+// if (text1.charAt(9) == "e" && text1.includes("Hello")) {
+//     alert(text1.concat(text2))
+// }
+// else { alert(text1) }
+
+//A task 5
+// let text = "Hello world,welcome to the welcome universe"
+// let result = text.indexOf("welcome");
+// alert(result);
+
+// A task 6
+// let text = "Hello world,welcome to the universe.";
+// alert(text.indexOf("e", 3));
+
+//A task 7
+// let text = "Hello planet earth,you are a great planet.";
+// let result = text.lastIndexOf("planet");
+// alert(result);
+
+//A task 8
+// let text = "dfrgrfd"
+// for (let k = 0; k < text.length; k++) {
+//     if (text.indexOf(text[k]) === text.lastIndexOf(text[k]))
+//         alert(text[k])
+//     break;
+// }
+
+//A task 9
+let text1 = prompt("Hello world")
+let letter = prompt("d")
+if (text1.indexOf(letter) !== -1) {
+    alert(text1.indexOf(letter))
+}
+else {
+    alert("chka")
+}
+// HomeWork: slice(),replace(),toUpperCase(),toLowerCase()
+
+
