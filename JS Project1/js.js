7abc916c7f363df7334cc9afdbc833b584015748
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 17:58:35 +0300

    Java Script Project1
    JavaScript

---
+++ JS Project1/js.js
@@ -0,0 +1,23 @@
+// alert("ok");
+// console.log("ok");
+// // document.write("JavaScript")
+// document.getElementById("h1").innerHTML = "<i><mark>JavaScript</mark></i>"
+// let a = 5n;
+// let b = "5";
+// console.log(typeof(10/0));
+// console.log(a == b);
+// let c;
+// console.log(c);
+// let user ={
+//     name:"Eva",
+//     age:30,
+//     countFriend:50n,
+//     surName:["Poghosyan", "Grigoryan"],
+//     img:true,
+// }
+
+// console.log(`My user is ${user.age} years old and she have a ${user.surName.length} surname`);
+// let v=Symbol("12ab");
+// let n=Symbol("12ab");
+// console.log(v === n);
+console.log(document.body);
