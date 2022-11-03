fe4f7c83f54fd5d462bbbbf93c929139803f866d
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 18:34:29 +0300

    Java Script Calculator

---
+++ JS Calculator/js.js
@@ -0,0 +1,80 @@
+function my(x) {
+    document.getElementById("inp1").value =
+        document.getElementById("inp1").value + x
+}
+function ac() {
+    document.getElementById("inp1").value = "";
+}
+function ravno() {
+    document.getElementById("inp1").value = eval(document.getElementById("inp1").value)
+}
+let a;
+function as() {
+    a = document.getElementById("inp1").value
+    document.getElementById("inp1").value=a.slice(0,a.length-1)
+}
+
+//A task1
+function but7(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"7";
+}
+function but8(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"8"
+}
+function but9(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"9"
+}
+function but4(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"4"
+}
+function but5(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"5"
+}
+function but6(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"6"
+}
+function minus(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"-"
+}
+function but1(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"1"
+}
+function but2(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"2"
+}
+function but3(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"3"
+}
+function plus(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"+"
+}
+function but0(){
+    document.getElementById("fon1").value =
+    document.getElementById("fon1").value+"0"
+}
+function point(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"."
+}
+function pm(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"+/-"
+}
+function divide(){
+    document.getElementById("inp1").value =
+    document.getElementById("inp1").value+"/"
+}
+function ravno(){
+    document.getElementById("inp1").value = "";
+}
