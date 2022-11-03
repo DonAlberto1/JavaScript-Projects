b3cadb8ffb5f272a747961285facc6cfb1c3c973
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 18:02:07 +0300

    Java Script Project3

---
+++ JS Project3/js.js
@@ -0,0 +1,14 @@
+//const select1 = document.querySelector("#select1");
+// for (let i = 1; i <= 31; i++) {
+// const option1 = document.createElement("option");
+// option1.innerText = i;
+// select1.append(option1);
+// }
+
+// const select3 = document.querySelector("#select2");
+// for (let i = 2000; i <= 2022; i++) {
+// const option2 = document.createElement("option");
+// option2.innerText = i;
+// select3.append(option2);
+// }
+
