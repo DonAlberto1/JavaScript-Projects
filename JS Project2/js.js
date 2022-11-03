ec99e18994f5ff1706cb5742dd5b7fa976629acf
DonAlberto1 <117360586+DonAlberto1@users.noreply.github.com>
2022-11-03 18:00:51 +0300

    Java Script Project2

---
+++ JS Project2/js.js
@@ -0,0 +1,190 @@
+// alert("ok");
+// console.log("ok");
+// // document.write("JavaScript")
+// document.getElementById("h1").innerHTML = "<i><mark>JavaScript</mark></i>"
+// let a = 5n;
+// let b = "5";
+// console.log(typeof(10/0));
+// console.log(a == b);
+// let c;
+// console.log(c);
+// let user ={
+//     name:"Eva",
+//     age:30,
+//     countFriend:50n,
+//     surName:["Poghosyan", "Grigoryan"],
+//     img:true,
+// }
+
+// console.log(`My user is ${user.age} years old and she have a ${user.surName.length} surname`);
+// let v=Symbol("12ab");
+// let n=Symbol("12ab");
+// console.log(v === n);
+// console.log(document.body);
+// console.log(8 % 3)//2;
+// console.log(4 ** 3)//4*4*4;
+// "use strict";
+// s = 5;
+// console.log(s);
+// console.log(5+"6");
+// let a="5";
+// if (a==="5"){}else{console.log(urrraaa)};
+//let n = prompt("");
+//if (n % 2 == 0) { console.log("True") } else { console.log("False") };
+
+//var string = 'The revolution will not be televised.';
+//string;
+//let n = prompt("What is your Name");
+//var one = console.log("My Name is") + console.log(n);
+//console.log("My Name is"+ n);
+
+//let a = prompt ();
+//let b = prompt ();
+//console.log(+a + +b);
+
+//let a = prompt("Inch ore aysor");
+//if (a == 16) { alert("Today") } else if (a < 16) { alert("This day was gone") }
+//else { alert("This day has not come") }
+
+//let a = +prompt("Write Number");
+//if (a === 1) { alert("One") } else if (a === 2) { alert("Two") }
+//else if (a === 3) { alert("Three") }
+//else if (a === 4) { alert("Four") } else if (a === 5) { alert("Five") }
+//else if (a === 6) { alert("Six") }
+//else if (a === 7) { alert("Seven") } else if (a === 8) { alert("Eight") }
+//else if (a === 9) { alert("Nine") } else if (a === 10) { alert("Ten") };
+
+//let a = prompt("Wright Type");
+//if (a == "Alert") { document.write("Alert") } else if (a == "Console") { document.write("Console") }
+//else if (a == "Log") { document.write("Log") };
+
+//let n = prompt();
+//if ((n >= 3 && n <= 5) || (n >= 8 && n <= 12)) { console.log(1); } else { console.log(2); };
+
+//let a = prompt();
+//let b = prompt();
+//let c = prompt();
+//if (a > b && a > c) { console.log(a) }
+//else if (b > a && b > c) { console.log(b) }
+//else if (c > a && c > b) { console.log(b) };
+
+//Home Work 8-15;
+
+//A task 8
+//let a = prompt();
+//let b = prompt();
+//let c = prompt();
+//let d = prompt();
+//if (a < b && a < c && a < d) { console.log(a); }
+//else if (b < a && b < c && b < d) { console.log(b); }
+//else if (c < a && c < b && c < d) { console.log(c); }
+//else if (d < a && d < b && d < c) { console.log(d); };
+
+//A task 9
+// let a = prompt();
+// let b = prompt();
+// let c = prompt();
+// let d = prompt();
+// if (a > b && a > c && a > d) { console.log("a="); console.log(a); }
+// else if (b > a && b > c && b > d) { console.log("b="); console.log(b); }
+// else if (c > a && c > b && c > d) { console.log("c="); console.log(c); }
+// else if (d > a && d > b && d > c) { console.log("d="); console.log(d); };
+
+//A task 10
+//let a = +prompt();
+//let b = +prompt();
+//let c = +prompt();
+//if ((a + b) > c) { console.log("Yes") }
+//else if ((a + c) < b) { console.log("Yes") }
+//else if ((b + c) < a) { console.log("Yes") } { console.log("No"); };
+
+//A task 11
+// let a = prompt("Какое «официальное» название JavaScript?")
+// if (a == "ECMAScript") { alert("Верно!") }
+// else { alert("Не знаете? ECMAScript!"); }
+
+//A task 12
+
+// let a = +prompt("Введите число:")
+// if (a > 0) { alert("1") }
+// else if (a < 0) { alert("-1") }
+// else { alert("0") };
+
+//A task 13
+//let a = +prompt()
+//let b = +prompt()
+//if (a + b < 4) { alert(result = 'Мало'); }
+//else if (a + b > 4) { alert(result = 'Много'); }
+
+//result = (a + b < 4) ? 'Мало' : 'Много';
+
+//A task 14
+//let a = prompt()
+//if (a == "Сотрудник") { alert("Привет") }
+//else if (a == "Директор") { alert("Здравствуйте") }
+//else { alert("Нет логина") };
+
+//A task 15(11)
+//let a = +prompt();
+//let b = +prompt();
+//let c = +prompt();
+//if (a = b && a + b > c) { alert("Yes") }
+//else if (a = c && a + c > b) { alert("Yes") }
+//else if (b = c && b + c > a) { alert("Yes") }
+//else { alert("No") }
+
+//A task 18(14) 
+//let a = +prompt();
+//let b = +prompt();
+//let c = +prompt();
+//if (a === 1) { alert("1") }
+//else if (b === 1) { alert("1") }
+//else if (c === 1) { alert("1") }
+//else { alert("0") }
+
+//A task 19(12)
+// let a = +prompt();
+// let b = +prompt();
+// if (a > 0 && b > 0) { alert("1") }
+// else { alert("0") }
+
+//A task 20(13)
+//let x = +prompt();
+//let y = +prompt();
+//if (x > 0 && y > 0) { alert("1") }
+//else if (x < 0 && y > 0) { alert("2") }
+//else if (x < 0 && y < 0) { alert("3") }
+//else if (x > 0 && y < 0) { alert("4") };
+
+// let text = prompt("add text 5 letters");
+// let n = 5;
+// for (let i = 0; i < 5; i++) {
+//     if (text[i] != text[4 - i]) {
+//         alert("Fales")
+//         break;
+//     } else { n-- }
+
+// }
+// if (n === 0) { alert("True") }
+
+// const div = document.createElement("div");
+// div.classList = "month";
+// const calendar = document.querySelector("#calendar");
+// calendar.append(div);
+
+//A Task 1:Calendar Code;
+for (let y = 2010; y <= 2022; y++) {
+    document.write(`<h1>${y}</h1>`);
+    document.write('<div class="year">');
+    for (let j = 0; j < 12; j++) {
+        document.write('<div class="month">');
+        document.write(`<h2>${j}</h2>`)
+        for (let i = 1; i <= 31; i++) {
+            document.write('<div class="day">');
+            document.write(i);
+            document.write("</div>");
+        }
+        document.write("</div>");
+    }
+    document.write("</div>");
+}
